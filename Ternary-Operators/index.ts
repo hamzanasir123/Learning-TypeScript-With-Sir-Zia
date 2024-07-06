1a5aa0c7c6d112d2f380690482486cedbc72e005
Hamza Nasir <161011944+hamzanasir123@users.noreply.github.com>
2024-07-06 15:36:45 +0500

    Add files via upload

---
+++ Ternary-Operators/index.ts
@@ -0,0 +1,17 @@
+let userName = "Hamza Nasir";
+
+if(userName == "Hamza Nasir"){
+    console.log("Hello Hamza");
+}else{
+    console.error("User Name Not Found");
+}
+
+let result = userName == "Hamza Nasir"
+? console.log("Hello Hamza")
+: console.error("User Name Not Found");
+
+// && And
+// || Or
+// ? IF
+// : ELSE
+// Both Are One Of Ternary Operators 
