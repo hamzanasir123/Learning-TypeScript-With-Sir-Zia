d5ba0360131840c75e992e34201b86c4529201a8
Hamza Nasir <161011944+hamzanasir123@users.noreply.github.com>
2024-07-06 15:36:06 +0500

    Add files via upload

---
+++ Variables/Class.ts
@@ -0,0 +1,83 @@
+// let name1 : string = 'Hamza';
+// const age  ='23';
+// var marraige = 'false';
+
+console.log('Hello world');
+
+let numberOfFaculty :number = 4;
+let numberOfManagment : number = 3;
+let numberOfAbsentFaculty = 2;
+let totalNumberOfFaculty = numberOfFaculty * 21;
+
+let biryaniForOnePerson = 42 / totalNumberOfFaculty;
+
+console.log(biryaniForOnePerson);
+
+let weightInKg = 70;
+let heightInMeters = 1.75;
+let bmi = weightInKg / (heightInMeters*heightInMeters);
+console.log(`your BMI is ${bmi}`);
+
+let firstName ='Hamza';
+let age = 23;
+let message = "Hello my name is: " + firstName + " and my age is: " + age;
+
+console.log(`Hello my name is ${firstName} and My age is ${age}`);
+
+let index = 10;
+
+console.log(index);
+
+let a = 5;
+let b = 8;
+let c = 6;
+
+
+
+//             11    +   12    + 8 +  11    - 2 
+let result = ++index + ++index + 8 + --index - 2;
+
+console.log(result);
+
+let message1 = 'Hello world';
+let num = 20;
+num += 2;
+num -= 2;
+// num = num -2
+
+let num1 = 33;
+let num2 = 34;
+console.log(num1 == num2);
+console.log(num1 != num2);
+console.log(num1 > num2);
+console.log(num1 < num2);
+
+let age1 = 18;
+console.log(age1 >= 18);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
