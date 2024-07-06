a3ad4a771aa6e03f3a66735684ba18dd91140362
Hamza Nasir <161011944+hamzanasir123@users.noreply.github.com>
2024-07-06 15:45:09 +0500

    Add files via upload

---
+++ If_else/Class_5.ts
@@ -0,0 +1,16 @@
+
+let percentage = 72;
+
+if (percentage <= 50){
+    console.log("You are fail");
+}else if(percentage >= 50 && percentage <= 59){
+    console.log("You are pass with grade C");
+}else if(percentage >= 60 && percentage <= 69){
+    console.log("You are pass with grade B");
+}else if(percentage >= 70 && percentage <= 79){
+    console.log("You are pass with grade A");
+}else if(percentage >= 80 ){
+    console.log("You are pass with grade A+");
+}else{
+    console.log("Please Try Again")
+}
