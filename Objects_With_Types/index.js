c948b8ed010740ea2135f8549c728aed1473a174
Hamza Nasir <161011944+hamzanasir123@users.noreply.github.com>
2024-07-06 15:44:23 +0500

    Add files via upload

---
+++ Objects_With_Types/index.js
@@ -0,0 +1,17 @@
+"use strict";
+let id = {
+    name: "Hamza Nasir",
+    age: 24,
+    class: "Thursday",
+    rollNumber: 254369
+};
+// console.log(`i am ${id.name},i am ${id.age} years old,i sit in a ${id.class} Class and My Roll No is ${id.rollNumber}`);
+// console.table(id);
+let car = {
+    name: "Honda Civic",
+    model: 2024,
+    colour: "Black",
+    engine: "2600cc",
+    milege: 40,
+};
+console.table(car);
---
+++ Objects_With_Types/index.ts
@@ -0,0 +1,34 @@
+
+// Object With Types given
+
+let id :{
+    name:string,
+    age:number,
+    class:string,
+    rollNumber:number,
+    isStudent:boolean
+}= {
+    name:"Hamza Nasir",
+    age:24,
+    class:"Thursday",
+    rollNumber:254369,
+    isStudent: true
+}
+//  console.log(`i am ${id.name},i am ${id.age} years old,i sit in a ${id.class} Class and My Roll No is ${id.rollNumber}`);
+console.table(id);
+
+
+
+
+// Basic Object
+// let car = {
+//     name:"Honda Civic",
+//     model:2024,
+//     colour:"Black",
+//     engine:"2600cc",
+//     milege: 40
+
+// }
+// console.table(car);
+
+
