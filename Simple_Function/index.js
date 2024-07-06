340b151860a1b5d3a8f9546617a5f6b78bac2969
Hamza Nasir <161011944+hamzanasir123@users.noreply.github.com>
2024-07-06 15:43:35 +0500

    Add files via upload

---
+++ Simple_Function/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// import inquirer from "inquirer";
+function tea(sugar) {
+    let cooked = sugar + 3 + 3 + 3;
+    return cooked;
+}
+let response = tea(2);
+console.log(response);
---
+++ Simple_Function/index.ts
@@ -0,0 +1,10 @@
+// import inquirer from "inquirer";
+
+function biryani (masala:any){
+  let cooked = masala + 3 + 3 + 3;
+  return cooked;
+}
+
+let response = biryani(2)
+console.log(response);
+
