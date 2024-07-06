02fe03e376ee84cccd9658b1379aa2a069b01c70
Hamza Nasir <161011944+hamzanasir123@users.noreply.github.com>
2024-07-06 15:37:07 +0500

    Add files via upload

---
+++ Switch-Cases/index.ts
@@ -0,0 +1,12 @@
+const myAge:any = 12;
+
+switch(true){
+case myAge <= 18 :
+console.log("You are allow to take an admission in Cricket Accademy");
+    break;
+case myAge <= 9 :
+    console.log("You are too young!");
+    break;
+default:
+console.log("You are too old!");
+}
